7d27ba56cdab479060584ce37f483c24d53396b3
mesic <mesic.canioni.jerome@gmail.com>
2021-08-11 18:01:55 +0200

    playing with mesh.scale

--- src/script.js
+++ src/script.js
@@ -13,15 +13,22 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
 // mesh.position.x = 0.7
 // mesh.position.y = -0.6
 // mesh.position.z = 1
-scene.add(mesh)
 
 mesh.position.set(0.7, - 0.6, 1)
 
+//SCALE
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+
 //AXES HELPER
-const axesHelper = new THREE.AxesHelper(2)
+const axesHelper = new THREE.AxesHelper()
 scene.add(axesHelper)
 
 
